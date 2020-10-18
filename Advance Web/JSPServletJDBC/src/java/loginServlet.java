318883490e830bdde9ebef4abae007834d88db19
Hafiz Shahman <hafizshahman1@gmail.com>
2020-10-18 21:58:07 +0800

    Update loginServlet.java

--- Advance Web/JSPServletJDBC/src/java/loginServlet.java
+++ Advance Web/JSPServletJDBC/src/java/loginServlet.java
@@ -56,8 +56,8 @@ public class loginServlet extends HttpServlet {
                         + user_id + "'");
                 if (!result.equals(null)) {
                     while (result.next()) {
-                        user_ID_from_DB = result.getString("user_id").trim();
-                        user_password_from_DB = result.getString("user_password").trim();
+                        user_ID_from_DB = result.getString("login_id").trim();
+                        user_password_from_DB = result.getString("password").trim();
                         System.out.println(user_ID_from_DB + " " + user_password_from_DB);
                     }
                     // Database operations completed
@@ -83,30 +83,6 @@ public class loginServlet extends HttpServlet {
         }
     }
 
-    /**
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-     * methods.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet loginServlet</title>");
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet loginServlet at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
-        }
-    }
+    
 
 }
