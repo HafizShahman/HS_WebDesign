8c4f9cf9d466e7a682825c52d69bc8c9f1d76706
Hafiz Shahman <hafizshahman1@gmail.com>
2020-11-13 00:41:49 +0800

    change

---
+++ Advance Web/OnlineBookStore/src/java/regServ.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author hafiz
+ */
+public class regServ extends HttpServlet {
+    
+    private static final long serialVersionUID = 102831973239L;
+    String Name = "";
+    String Address = "";
+    String PhoneNo = "";
+    String Email = "";
+    String Password = "";
+
+}
